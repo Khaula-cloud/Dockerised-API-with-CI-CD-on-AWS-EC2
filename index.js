f7f816451e2cd4aa4b638cb4e7cc6317fcf6a9d6
Khaula-cloud <khaulaazhar828@gmail.com>
2026-04-23 11:13:35 +0500

    Dockerized the App

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let todos = [];
+
+app.get('/', (req, res) => res.json({ status: 'ok' }));
+app.get('/todos', (req, res) => res.json(todos));
+app.post('/todos', (req, res) => {
+  const todo = { id: Date.now(), text: req.body.text };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+app.delete('/todos/:id', (req, res) => {
+  todos = todos.filter(t => t.id !== Number(req.params.id));
+  res.json({ message: 'deleted' });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
